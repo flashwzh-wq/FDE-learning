2b1e9fad0fe65eb0a87d5e170e588a9db521898a
Wu Zehua <flash_wzh@gmail.com>
2026-08-14 11:13:34 +0800

    w5练习存

---
+++ review_dict_list.py
@@ -0,0 +1,12 @@
+scores = []
+record = {}
+scores.append(88)              # append 是动作，加括号，一次一个
+scores.append(79)
+scores.append(92)
+record["万达"] = 88
+record["天虹"] = 79
+record["永旺"] = 92
+print(record["永旺"])  
+print(scores[1])
+print(scores)                  # 看列表长啥样
+print(record)                  # 看字典长啥样
